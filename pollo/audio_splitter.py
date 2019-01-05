f4bf4e93dd6e0f8113dda69d0b7878385c2be6ab
Lobarr <jesulobaegunjobi@hotmail.com>
2019-01-05 14:29:30 -0500

    implemented stereo to audio convertion pre-split

--- pollo/audio_splitter.py
+++ pollo/audio_splitter.py
@@ -3,13 +3,14 @@ import os
 import string
 from pydub import AudioSegment
 
+LIMIT = 45 # splits into 45 seconds chunks - can handle 3 convertions per minute with this
 class AudioSplitter:
   """
   Splits audio file into minute long chunks
   """
   def __init__(self, fn: str):
     if fn.index('.wav') > 0:
-      self.__audio = AudioSegment.from_wav(fn)
+      self.__audio = AudioSegment.from_wav(fn).set_channels(1) # converts to mono as google api supports just mono
       self.__fn = fn
 
   def split(self) -> list:
@@ -17,16 +18,16 @@ class AudioSplitter:
     duration = self.__audio.duration_seconds
     while True:
       split_fn = os.path.join(os.path.dirname(os.path.abspath(self.__fn)), f'{uuid.uuid4()}.wav')
-      if (duration / 60) > 1:
-        minute = 60 * 1000 #ms to seconds 
-        split = self.__audio[:minute] 
+      if (duration / LIMIT) > 1:
+        limit_ms = LIMIT * 1000 # seconds to ms 
+        split = self.__audio[:limit_ms] 
         split.export(split_fn, format='wav')
         split_audios.append(split_fn)
       else:
-        remainder =  int(self.__audio.duration_seconds/60) * 60 * 1000
+        remainder =  int(self.__audio.duration_seconds/LIMIT) * 60 * 1000
         split = self.__audio[remainder:]
         split.export(split_fn, format='wav')
         split_audios.append(split_fn)
         break
-      duration = duration-60
+      duration = duration-LIMIT
     return split_audios

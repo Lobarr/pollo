23468f2310facab5e0f3028ef2f91debd032fdbe
Lobarr <jesulobaegunjobi@hotmail.com>
2019-06-09 06:19:13 -0400

    refactor: ci start

--- tests/test_transcriber.py
+++ tests/test_transcriber.py
@@ -1,9 +1,4 @@
-import sys
 import os
-
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
-
 import mock
 from mock import patch
 from google.cloud import speech
--- tests/test_video_converter.py
+++ tests/test_video_converter.py
@@ -1,9 +1,4 @@
-import sys
 import os
-
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
-
 import pytest
 import mock
 import ffmpeg

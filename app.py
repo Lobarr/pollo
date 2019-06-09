561838be8fb33e788e884706a14dc870ec7e11f7
Lobarr <jesulobaegunjobi@hotmail.com>
2019-06-09 13:20:37 -0400

    feat: added cors to backend

--- app.py
+++ app.py
@@ -2,16 +2,36 @@ import os
 import time
 import asyncio
 import socketio
+import logging
+import aiohttp_cors
 from aiohttp import web
 
 #routers
 from api import ping_router, transribe_router, sio
 
+is_prod = os.getenv("ENV") is "prod"
+
 if __name__ == "__main__":
   app = web.Application()
-  sio.attach(app)
+  logging.basicConfig(level=logging.INFO)
+  logger = logging.getLogger("API")
+
+
   app.add_routes(ping_router)
   app.add_routes(transribe_router)
-  print(asyncio.get_event_loop().is_running())
-  web.run_app(app, port=os.getenv("PORT", 3000))
- 
+  app.add_routes([web.static('/app', './client/dist')]) if is_prod else None
+
+  cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+  })
+  for route in list(app.router.routes()):
+    cors.add(route)
+
+  sio.attach(app)
+   
+  web.run_app(app, port=os.getenv("PORT", 3000), access_log=logger)
+  
